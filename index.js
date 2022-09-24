bc80521da1545137a2c5f0ee3c7d0176e3b5ea86
Amarnath Barpanda <47183341+amarnathbarpanda@users.noreply.github.com>
2022-09-25 00:11:22 +0530

    basic folder structure and dependencies added

---
+++ index.js
@@ -0,0 +1,22 @@
+//getting all the libraries and modules
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const port = 8000;
+
+// we need to fire up the express as a funcion to use all the functionalities of express
+
+const app = express();
+
+//middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res)=>{
+    res.send('Hey node server is running');
+});
+
+// listening to port
+app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
+});
